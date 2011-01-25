ebf74d1c8c45e3933b43a3a9621a910055659868
Daniele Gobbetti <daniele.gobbetti@gmail.com>
2011-01-25 11:54:13 +0100

    unescapeHtml moved here

--- src/org/mixare/data/Json.java
+++ src/org/mixare/data/Json.java
@@ -19,6 +19,7 @@
 package org.mixare.data;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -27,6 +28,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.mixare.Marker;
+import org.mixare.MixContext;
 import org.mixare.MixView;
 import org.mixare.POIMarker;
 import org.mixare.SocialMarker;
@@ -37,7 +39,7 @@ import android.util.Log;
 public class Json extends DataHandler {
 
 	public static final int MAX_JSON_OBJECTS=1000;
-
+	
 	public List<Marker> load(JSONObject root, DATAFORMAT dataformat) {
 		JSONObject jo = null;
 		JSONArray dataArray = null;
@@ -150,8 +152,9 @@ public class Json extends DataHandler {
 			if(jo.has("has_detail_page") && jo.getInt("has_detail_page")!=0 && jo.has("webpage"))
 				link=jo.getString("webpage");
 			
+			
 			ma = new POIMarker(
-					jo.getString("title"), 
+					unescapeHTML(jo.getString("title"), 0), 
 					jo.getDouble("lat"), 
 					jo.getDouble("lng"), 
 					jo.getDouble("elevation"), 
@@ -169,7 +172,7 @@ public class Json extends DataHandler {
 			Log.v(MixView.TAG, "processing Wikipedia JSON object");
 	
 			ma = new POIMarker(
-					jo.getString("title"), 
+					unescapeHTML(jo.getString("title"), 0), 
 					jo.getDouble("lat"), 
 					jo.getDouble("lng"), 
 					jo.getDouble("elevation"), 
@@ -177,5 +180,73 @@ public class Json extends DataHandler {
 					DataSource.DATASOURCE.WIKIPEDIA);
 		}
 		return ma;
+	}
+	
+	private static HashMap<String, String> htmlEntities;
+	static {
+		htmlEntities = new HashMap<String, String>();
+		htmlEntities.put("&lt;", "<");
+		htmlEntities.put("&gt;", ">");
+		htmlEntities.put("&amp;", "&");
+		htmlEntities.put("&quot;", "\"");
+		htmlEntities.put("&agrave;", "à");
+		htmlEntities.put("&Agrave;", "À");
+		htmlEntities.put("&acirc;", "â");
+		htmlEntities.put("&auml;", "ä");
+		htmlEntities.put("&Auml;", "Ä");
+		htmlEntities.put("&Acirc;", "Â");
+		htmlEntities.put("&aring;", "å");
+		htmlEntities.put("&Aring;", "Å");
+		htmlEntities.put("&aelig;", "æ");
+		htmlEntities.put("&AElig;", "Æ");
+		htmlEntities.put("&ccedil;", "ç");
+		htmlEntities.put("&Ccedil;", "Ç");
+		htmlEntities.put("&eacute;", "é");
+		htmlEntities.put("&Eacute;", "É");
+		htmlEntities.put("&egrave;", "è");
+		htmlEntities.put("&Egrave;", "È");
+		htmlEntities.put("&ecirc;", "ê");
+		htmlEntities.put("&Ecirc;", "Ê");
+		htmlEntities.put("&euml;", "ë");
+		htmlEntities.put("&Euml;", "Ë");
+		htmlEntities.put("&iuml;", "ï");
+		htmlEntities.put("&Iuml;", "Ï");
+		htmlEntities.put("&ocirc;", "ô");
+		htmlEntities.put("&Ocirc;", "Ô");
+		htmlEntities.put("&ouml;", "ö");
+		htmlEntities.put("&Ouml;", "Ö");
+		htmlEntities.put("&oslash;", "ø");
+		htmlEntities.put("&Oslash;", "Ø");
+		htmlEntities.put("&szlig;", "ß");
+		htmlEntities.put("&ugrave;", "ù");
+		htmlEntities.put("&Ugrave;", "Ù");
+		htmlEntities.put("&ucirc;", "û");
+		htmlEntities.put("&Ucirc;", "Û");
+		htmlEntities.put("&uuml;", "ü");
+		htmlEntities.put("&Uuml;", "Ü");
+		htmlEntities.put("&nbsp;", " ");
+		htmlEntities.put("&copy;", "\u00a9");
+		htmlEntities.put("&reg;", "\u00ae");
+		htmlEntities.put("&euro;", "\u20a0");
+	}
+
+	public String unescapeHTML(String source, int start) {
+		int i, j;
+
+		i = source.indexOf("&", start);
+		if (i > -1) {
+			j = source.indexOf(";", i);
+			if (j > i) {
+				String entityToLookFor = source.substring(i, j + 1);
+				String value = (String) htmlEntities.get(entityToLookFor);
+				if (value != null) {
+					source = new StringBuffer().append(source.substring(0, i))
+					.append(value).append(source.substring(j + 1))
+					.toString();
+					return unescapeHTML(source, i + 1); // recursive call
+				}
+			}
+		}
+		return source;
 	}
 }
