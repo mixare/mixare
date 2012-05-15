8d7a8fa2bdc01d90493fce4d9505abb195b1dd5e
Abdullahi Egal <abdu.egal@gmail.com>
2012-05-15 11:26:20 +0200

    Added some more license information

--- plugins/mixare-library/src/org/mixare/lib/marker/draw/DrawTextBox.java
+++ plugins/mixare-library/src/org/mixare/lib/marker/draw/DrawTextBox.java
@@ -1,8 +1,20 @@
-/**
- * A draw command that can be send by a plugin marker to draw an arrow on the client.
- * This class extends the drawcommand, that stores the properties, so that it can be
- * transfered to the client.
- * @author A. Egal
+/*
+ * Copyright (C) 2012- Peer internet solutions & Finalist IT Group
+ * 
+ * This file is part of mixare.
+ * 
+ * This program is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ * for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>
  */
 package org.mixare.lib.marker.draw;
 
